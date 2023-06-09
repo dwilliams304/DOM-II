ab0a8866aa9bfa8bb9cbb3d25996adaeeadf8e95
dwilliams304 <d.williams0434@gmail.com>
2023-06-08 23:24:09 -0700

    Add hover function to about button, secret background color changes from key presses

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import './less/index.less'
 
 // Your code goes here!
- const homeBtn = document.querySelector('.nav-link');
-
-
- homeBtn.addEventListener('dblclick', magicSchoolBus);
-
-
- function magicSchoolBus(){
-    const imgToChange = document.querySelector('.intro img');
-    imgToChange.src = 'https://occ.a.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWz4_LTOyo9T4OysXT6jnEoFdWVTfQxRcb3bryLHaOpUEfrP-bju1POiFlz4GvmXpvlJ196VTiir7GlqGQTOPj5Eq59pz6sWLsiu.jpg?r=08f';
-    const titleToChange = document.querySelector('.logo-heading');
-    titleToChange.textContent = 'Magic School Bus'
+const body = document.querySelector('body');
+const homeBtn = document.querySelector('.nav-link');
+homeBtn.addEventListener('dblclick', magicSchoolBus);
+function magicSchoolBus(){
+   const imgToChange = document.querySelector('.intro img');
+   imgToChange.src = 'https://occ.a.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWz4_LTOyo9T4OysXT6jnEoFdWVTfQxRcb3bryLHaOpUEfrP-bju1POiFlz4GvmXpvlJ196VTiir7GlqGQTOPj5Eq59pz6sWLsiu.jpg?r=08f';
+   const titleToChange = document.querySelector('.logo-heading');
+   titleToChange.textContent = 'Magic School Bus'
 }
+
+const aboutBtn = document.querySelector('.nav-link:nth-child(2)');
+aboutBtn.addEventListener('mouseover', () => {
+    aboutBtn.textContent = 'Dont hover over me!';
+})
+
+document.addEventListener('keydown', (e) => {
+    const key = e.key;
+    if(key === 'b'){
+        body.style.backgroundColor = 'cyan';
+    }
+    else if(key === 'r'){
+        body.style.backgroundColor = 'red';
+    }
+    else if(key === 'g'){
+        body.style.backgroundColor = 'green';
+    }
+    else{
+        body.style.backgroundColor = 'white';
+    }
+})
